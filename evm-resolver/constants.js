e30f2ba2424b4768c05b26bedcc03f4e8070f75a
ahsueh1996 <ahsueh1996@gmail.com>
2025-09-28 03:55:29 +0530

    add evm-resolver (base example)

---
+++ evm-resolver/constants.js
@@ -0,0 +1,22 @@
+module.exports = {
+  rpc: 'https://sepolia.base.org',
+  limitOrderProtocol: "0xE53136D9De56672e8D2665C98653AC7b8A60Dc44",
+  domain: {
+    name: "1inch Limit Order Protocol",
+    version: "4",
+    chainId: "84532",
+    verifyingContract: "0xE53136D9De56672e8D2665C98653AC7b8A60Dc44",
+  },
+  
+  base_sepolia: {
+    escrowFactory: "0x2893441ba71e3f2f854fe56f2a2b91a45820b0f5",
+    FeeBank: "0xA8277e8758058780e3C014ec02f3D555c16ed425",
+    escrowSrcImpl: "0xc84aA0f22eAcdAE4Aa343E20feAaD5bc742F22e9",
+    escrowDstImpl: "0x45a3Bc4e8E6AeB4b50C31b6A6E0898bff33164ae",
+    ERC20_TRUE: "0x384229e3d543ccbd869987303246b1b602323d33",
+    USDC: "0x036cbd53842c5426634e7929541ec2318f3dcf7e",
+},
+  // deployed from deployResolver.js
+  resolverAddress: "YOUR_RESOLVER_ADDRESS_HERE" 
+  
+};
